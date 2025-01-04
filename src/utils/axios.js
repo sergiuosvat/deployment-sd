ce61e05f92a6fbbf1661972b53a20a88d9aeda7e
sergiuosvat <sergiuosvat@yahoo.com>
2025-01-04 19:55:13 +0200

    update urls

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -7,12 +7,12 @@ const commonHeaders = {
 };
 
 const instance_users = axios.create({
-    baseURL: "http://localhost/userapi",
+    baseURL: "https://user-management-microservice.proudgrass-626b941a.westeurope.azurecontainerapps.io/userapi",
     headers: commonHeaders
 });
 
 const instance_devices = axios.create({
-    baseURL: "http://localhost/deviceapi",
+    baseURL: "https://device-management-microservice.proudgrass-626b941a.westeurope.azurecontainerapps.io/deviceapi",
     headers: commonHeaders
 });
 
